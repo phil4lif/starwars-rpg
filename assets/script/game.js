4becfd8f3f44a1089c504b67562b62d0e594ad7c
phil cowan <philipjcowan1@gmail.com>
2019-12-24 12:34:22 -0700

    added hp labels to the characters and formatted it, writing pseudo code for script page

---
+++ assets/script/game.js
@@ -0,0 +1,13 @@
+//the user will choose their character and then it moves to the your character area
+
+
+// the characters that were not chosen will move to the choose your opponent area
+
+// the user then clicks the attack button, then an amount of damage will be done based on how many times the user has attacked this opponent, this amount will be reduced from the opponents hp
+
+
+//the opponent will automatically counter attack after it has taken damage and the amount of damage it does will be reduced from the users character's hp
+
+//if the user defeats the opponent, they choose the next opponent to fight and the process is repeated
+
+//when the user's character runs out of hp, their character has been defeated and they have lost the game.
